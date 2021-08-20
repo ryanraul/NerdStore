828783d4d5a5e6d335d57322516ec050cf89701a
Raul Ryan <ryanraul_11@hotmail.com>
2021-08-20 15:35:02 -0300

    Persisting with repositories - Creation and implementing ProdutoRepository - Implementing tables mapping - Implementing CatalogoContext [DataContext]

---
+++ src/NerdStore.Catalogo.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+
+        public ProdutoRepository(CatalogoContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+            => await _context.Produtos.AsNoTracking().ToListAsync();
+
+        public async Task<Produto> ObterPorId(Guid id)
+            => await _context.Produtos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+
+        public async Task<IEnumerable<Categoria>> ObterCategorias(int codigo)
+            => await _context.Categorias.AsNoTracking().ToListAsync();
+
+        public async Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+            => await _context.Produtos.AsNoTracking().Include(p => p.Categoria).Where(c => c.Categoria.Codigo == codigo).ToListAsync();
+
+        public void Adicionar(Produto produto)
+            => _context.Add(produto);
+
+        public void Atualizar(Produto produto)
+            => _context.Update(produto);
+
+        public void Adicionar(Categoria categoria)
+            => _context.Add(categoria);
+
+        public void Atualizar(Categoria categoria)
+            => _context.Update(categoria);
+
+        public void Dispose()
+            => _context?.Dispose();
+    }
+}
--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,4 +1,5 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System.Collections.Generic;
 
 namespace NerdStore.Catalogo.Domain
 {
@@ -6,6 +7,10 @@ namespace NerdStore.Catalogo.Domain
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
 
         public Categoria(string nome, int codigo)
         {
